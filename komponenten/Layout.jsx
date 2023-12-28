3636f95e6ddfcb31cdbdf45be8737fa5df9bac3d
Tim Guske <108478494+TimGuske@users.noreply.github.com>
2023-12-28 16:08:26 +0100

    resp design

--- komponenten/Layout.jsx
+++ komponenten/Layout.jsx
@@ -12,6 +12,7 @@ export default function Layout({children}) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navigation />
+      <br />
       <div className='container'>
         {children}
       </div>
--- komponenten/Slider.jsx
+++ komponenten/Slider.jsx
@@ -1,19 +1,15 @@
 import { Carousel } from "react-bootstrap"
 import Image from "next/image"
 
-
 export default function Slider() {
     return (
-        <div style={{ maxHeight: "500px", overflow: "hidden" }}>
-            <Carousel controls={false} fade={true} interval={2000} style={{ height: "100%" }}>
-                <Carousel.Item style={{ height: "100%" }}>
-                    <Image className="d-block w-100 rounded-3" src='/bilder/essen/burger.jpg' alt="burger"  width={3000} height={500} />
+        <div>
+            <Carousel controls={false} fade={true} interval={2000}>
+                <Carousel.Item>
+                    <Image className="d-block w-100 h-100 rounded-5" src='/bilder/essen/pizza.jpg' alt="pizza" width={3000} height={1000} />
                 </Carousel.Item>
-                <Carousel.Item style={{ height: "100%" }}>
-                    <Image className="d-block w-100 rounded-3" src='/bilder/essen/pizza.jpg' alt="pizza" width={1500} height={500} />
-                </Carousel.Item>
-                <Carousel.Item style={{ height: "100%" }}>
-                    <Image className="d-block w-100 rounded-3" src='/bilder/essen/burrito.jpg' alt="burrito" width={1500} height={500} />
+                <Carousel.Item>
+                    <Image className="d-block w-100 h-100 rounded-5" src='/bilder/essen/burrito.jpg' alt="burrito"  width={3000} height={1000} />
                 </Carousel.Item>
             </Carousel>
         </div>
