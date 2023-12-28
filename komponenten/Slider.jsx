9fb07dae6b6ae2f008f478b333ce60dd09fbfe23
Tim Guske <108478494+TimGuske@users.noreply.github.com>
2023-12-28 15:11:07 +0100

    bilder

--- komponenten/Slider.jsx
+++ komponenten/Slider.jsx
@@ -4,16 +4,16 @@ import Image from "next/image"
 
 export default function Slider() {
     return (
-        <div>
-            <Carousel controls={false} fade={true} interval={2000}>
-                <Carousel.Item>
-                    <Image className="d-block w-100 h-auto rounded-3" src='/bilder/essen/burger.jpg' alt="burger" width={3000} height={1000} />
+        <div style={{ maxHeight: "500px", overflow: "hidden" }}>
+            <Carousel controls={false} fade={true} interval={2000} style={{ height: "100%" }}>
+                <Carousel.Item style={{ height: "100%" }}>
+                    <Image className="d-block w-100 rounded-3" src='/bilder/essen/burger.jpg' alt="burger"  width={3000} height={500} />
                 </Carousel.Item>
-                <Carousel.Item>
-                    <Image className="d-block w-100 h-auto rounded-3" src='/bilder/essen/pizza.jpg' alt="pizza" width={3000} height={1000} />
+                <Carousel.Item style={{ height: "100%" }}>
+                    <Image className="d-block w-100 rounded-3" src='/bilder/essen/pizza.jpg' alt="pizza" width={1500} height={500} />
                 </Carousel.Item>
-                <Carousel.Item>
-                    <Image className="d-block w-100 h-auto rounded-3" src='/bilder/essen/burrito.jpg' alt="burrito" width={3000} height={1000} />
+                <Carousel.Item style={{ height: "100%" }}>
+                    <Image className="d-block w-100 rounded-3" src='/bilder/essen/burrito.jpg' alt="burrito" width={1500} height={500} />
                 </Carousel.Item>
             </Carousel>
         </div>
