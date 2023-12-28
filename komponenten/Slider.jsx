6595762efc1e2f2d00f99d263045057098a56368
Tim Guske <108478494+TimGuske@users.noreply.github.com>
2023-12-28 16:47:26 +0100

    slider

--- komponenten/Slider.jsx
+++ komponenten/Slider.jsx
@@ -7,9 +7,17 @@ export default function Slider() {
             <Carousel controls={false} fade={true} interval={2000}>
                 <Carousel.Item>
                     <Image className="d-block w-100 h-100 rounded-5" src='/bilder/essen/pizza.jpg' alt="pizza" width={3000} height={1000} />
+                    <Carousel.Caption>
+                        <h3>Lecker Essen bei Tim bestellen</h3>
+                        <p>Alles Nio und Regional</p>
+                    </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <Image className="d-block w-100 h-100 rounded-5" src='/bilder/essen/burrito.jpg' alt="burrito"  width={3000} height={1000} />
+                    <Carousel.Caption>
+                        <h3>Diverse schöne Sachen</h3>
+                        <p>Besser als jeder Italiener um die Ecke</p>
+                    </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
         </div>
