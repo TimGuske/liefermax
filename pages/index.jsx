90228d8a8e92c77f494383ec23f6874d3d2381cd
Tim Guske <108478494+TimGuske@users.noreply.github.com>
2023-12-28 13:36:05 +0100

    produktliste

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,11 @@
+import Produktliste from './komponenten/Produktliste'
 import Slider from './komponenten/Slider'
 
 export default function Home() {
   return (
     <div>
       <Slider />
-      <h1>Produkte</h1>
+      <Produktliste />
     </div>
   )
 }
---
+++ pages/komponenten/Produktliste.jsx
@@ -0,0 +1,8 @@
+
+export default function Produktliste() {
+  return (
+    <div>
+
+    </div>
+  )
+}
