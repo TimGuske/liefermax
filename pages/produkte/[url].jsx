dbd9abd5989323cd3c18a1bba73bb100259a2cd1
Tim Guske <108478494+TimGuske@users.noreply.github.com>
2023-12-29 19:13:01 +0100

    db connect

--- pages/produkte/[url].jsx
+++ pages/produkte/[url].jsx
@@ -1,61 +1,72 @@
-import {useRouter} from 'next/router';
-import jsondb from '@/jsondb/produktliste';
-import Link from 'next/link';
+import Link from 'next/link'
 import Image from 'next/image'
-import { ListGroup, Button , ListGroupItem} from 'react-bootstrap';
+import { ListGroup, Button, ListGroupItem } from 'react-bootstrap';
+import mongodb from '../../utils/mongodb';
+import Produkt from '../../models/Produkt';
 
-export default function Produktseite() {
-    const router = useRouter();
-    const {url} = router.query;
-    const produkt = jsondb.produkte.find((pr) => pr.url === url);
+export default function Produktseite({produkt}) {
 
     if (!produkt) {
         return (
             <div>
-                <h2>Produkt nicht gefunden</h2>
+                <h2>
+                    Produkt nicht vorhanden
+                </h2>
             </div>
         )
     }
-
-  return (
-    <div>
-    <div>
-        <Link href="/" className='text-dark'>
-             ← zurück zur Übersicht
-        </Link>
-    </div>
-        <div className='row row-cols-2 mt-2'>
+    return (
+        <div>
             <div>
-                <Image className='rounded-3' src={produkt.bild} alt={produkt.name} width={600} height={600} layout='responsive' />
+                <Link href="/" className='text-dark'>       
+                    ← zurück zur Übersicht
+                </Link>
             </div>
-            <div>
-                <h1>
-                    {produkt.name}
-                </h1>
-                <ListGroup variant='flush'>
-                    <ListGroupItem>
-                        <h2 className='text-danger'>{produkt.preis} €</h2>
-                    </ListGroupItem>
-                    <ListGroupItem>
-                        {produkt.beschreibung}
-                    </ListGroupItem>
-                    <ListGroupItem>
-                        Extras:
-                        doppelt <input className='form-check-input me-2' type="checkbox" />
-                        extra Pommes <input className='form-check-input me-2' type="checkbox" />
-                    </ListGroupItem>
-                    <ListGroupItem>
-                        <input className='form-control w-50' type="number" placeholder='1' min='1' ></input>
-                    </ListGroupItem>
-                    <ListGroupItem>
-                        <div className='row shadow'>
-                            <Button variant='danger'>zum Warenkorb</Button>
-                        </div>
-                    </ListGroupItem>
-                </ListGroup>
+            <div className='row row-cols-2 mt-2'>
+                <div>
+                    <Image className='rounded-3' src={produkt.bild} alt={produkt.name} width={600} height={600} layout='responsive' />
+                </div>
+                <div>
+                    <h1>
+                        {produkt.name}
+                    </h1>
+                    <ListGroup variant='flush'>
+                        <ListGroupItem>
+                            <h2 className='text-danger'>{produkt.preis} €</h2>
+                        </ListGroupItem>
+                        <ListGroupItem>
+                            {produkt.beschreibung}
+                        </ListGroupItem>
+                        <ListGroupItem>
+                            {produkt.extras.length ? "Extras: " : <p></p>}
+                            {produkt.extras.map((extra)=>(
+                                <span key={extra.name}>
+                                  {extra.text} <input className='form-check-input me-2' type="checkbox" />
+                                </span>
+                            ))}
+                        </ListGroupItem>
+                        <ListGroupItem>
+                            <input className='form-control w-50' type="number" placeholder='1' min='1' ></input>
+                        </ListGroupItem>
+                        <ListGroupItem>
+                            <div className='row shadow'>
+                                <Button variant='danger'>zum Warenkorb</Button>
+                            </div>
+                        </ListGroupItem>
+                    </ListGroup>
+                </div>
             </div>
         </div>
-    </div>
-  )
-
+    )
 }
+
+export async function getServerSideProps(context){
+    const url = context.params.url;
+    await mongodb.dbConnect();
+    const produkt = await Produkt.findOne({url}).lean();
+    return {
+      props: {
+        produkt: JSON.parse(JSON.stringify(produkt))
+      }
+    }
+  }
