b9d40d2990c10a98479e5c48a22d549d3aa6588e
Tim Guske <108478494+TimGuske@users.noreply.github.com>
2024-01-06 17:34:04 +0100

    id in Produktliste

--- komponenten/Produktliste.jsx
+++ komponenten/Produktliste.jsx
@@ -1,19 +1,21 @@
-import {Card, Button} from 'react-bootstrap'
-import Link from 'next/link'
-import { addProdukte } from '../redux/warenkorbSlice'
-import { useDispatch, useSelector } from 'react-redux'
-import { setName } from '../redux/infoSlice'
+import {Card, Button} from 'react-bootstrap';
+import Link from 'next/link';
+import { addProdukte } from '../redux/warenkorbSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { setName } from '../redux/infoSlice';
+import { v4 as uuidv4 } from 'uuid';
 
 export default function ProduktListe({ produkte }) {
 
     const dispatch = useDispatch();
-
+    const _id = uuidv4();
     const zumWarenkorb = (produkt) =>{
         console.log('Dispatcher aufgerufen für', produkt);
+        
         //const prod = produkte[0];
         //prod.preis = 99;
         //prod.menge = 4;
-        dispatch(addProdukte({...produkt, menge:1, extras:[]}));
+        dispatch(addProdukte({...produkt, menge:1, extras:[], _id}));
         dispatch(setName('Tim'));
     }
 
