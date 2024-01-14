45c6ccc6b19e3d94276833fb216d174ef2fef638
Tim Guske <108478494+TimGuske@users.noreply.github.com>
2024-01-14 14:48:59 +0100

    final

--- komponenten/Fusszeile.jsx
+++ komponenten/Fusszeile.jsx
@@ -1,7 +1,7 @@
 export default function Fusszeile() {
     return (
       <div className="d-flex justify-content-center fixed-bottom text-secondary bg-light">
-        <h6>🍔 Tims Lieferservice | ☎️ 0189 939222</h6>
+        <h6>🍔 Tims Lieferservice | ☎️ 0189 939222 | Produktion</h6>
       </div>
     )
   }
